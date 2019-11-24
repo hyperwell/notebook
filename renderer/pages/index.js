edc816db0af0bc12c5724faa6d968db4df776b55
Jan Kaßel <jan@kassel.works>
2019-11-24 16:10:13 +0100

    add nextron skeleton

---
+++ renderer/pages/index.js
@@ -0,0 +1,61 @@
+import electron from 'electron'
+import React, {useState, useEffect} from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+// prevent SSR webpacking
+const ipcRenderer = electron.ipcRenderer || false
+
+const Home = () => {
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const onChange = e => setMessage(e.target.value)
+  const onSubmit = e => {
+    e.preventDefault()
+    if (ipcRenderer) {
+      ipcRenderer.send('add-message', message)
+      setMessages([...messages, message])
+      setMessage('') // clear the input value
+    }
+  }
+
+  useEffect(() => {
+    // componentDidMount()
+    if (ipcRenderer) {
+      setMessages(ipcRenderer.sendSync('get-messages'))
+    }
+
+    return () => {
+      // componentWillUnmount()
+    }
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Home - Nextron (store-data)</title>
+      </Head>
+      <div>
+        <p>
+          ⚡ Electron + Next.js ⚡ -
+          <Link href="/next">
+            <a>Go to next page</a>
+          </Link>
+        </p>
+        <hr />
+        <h2>Enter your message:</h2>
+        <form onSubmit={onSubmit}>
+          <input type="text" value={message} onChange={onChange} />
+        </form>
+        <ul>
+          {messages.map((m, i) => (
+            <li key={i}>{m}</li>
+          ))}
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Home
