46e5ba784223eb9ed4aad80095dd6b03b6fa6aac
Jan Kaßel <jan@kassel.works>
2020-03-29 22:25:52 +0200

    refactor UI

---
+++ renderer/components/Toolbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Toolbar.module.css'
+
+export default function Toolbar() {
+  return (
+    <div className={styles.container}>
+      <p>hi</p>
+    </div>
+  )
+}
